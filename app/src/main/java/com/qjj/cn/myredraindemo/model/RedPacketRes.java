091ac976bc2136b1d743a0fa00f13652097901ad
nanfeng <yngshirong@log56.com>
2020-04-13 15:31:11 +0800

    资源替换

--- app/src/main/java/com/qjj/cn/myredraindemo/model/RedPacketRes.java
+++ app/src/main/java/com/qjj/cn/myredraindemo/model/RedPacketRes.java
@@ -20,14 +20,4 @@ public class RedPacketRes {
             R.mipmap.img_red_packet_boom_4,
             R.mipmap.img_red_packet_boom_5,
     };
-
-    /*爆炸列表*/
-    public static final int[] BOOM_LIST_BT = {
-            R.mipmap.img_red2_packet_boom_1,
-            R.mipmap.img_red2_packet_boom_2,
-            R.mipmap.img_red2_packet_boom_3,
-            R.mipmap.img_red2_packet_boom_4,
-            R.mipmap.img_red2_packet_boom_5,
-    };
-
 }
